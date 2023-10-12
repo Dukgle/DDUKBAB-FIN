8682c035bb0aafa05b2a7283ce86a982907dfbec
dustnrlck <dustnrlck@naver.com>
2023-10-13 00:55:43 +0900

    [FIX] 좌석 예약 시 색 변경

--- src/reservation/ResModal.jsx
+++ src/reservation/ResModal.jsx
@@ -1,30 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "react-modal";
+import "./ResModal.css";
 
 Modal.setAppElement("#root");
 
-function OneSeatModal({ isOpen, closeModal, content, handleReservation }) {
+function ResModal({ isOpen, closeModal, content, onReserve }) {
   const [studentId, setStudentId] = useState("");
+  const [isChairReserved, setIsChairReserved] = useState(false); // 추가: 의자 예약 상태
+
+  // 이펙트를 사용하여 예약 여부에 따라 의자 색상을 업데이트
+  useEffect(() => {
+    setIsChairReserved(true);
+  }, [content]);
 
   const handleStudentIdChange = (e) => {
     setStudentId(e.target.value);
   };
 
   const handleReserveClick = () => {
-    // 학번을 사용하여 예약 로직을 처리할 수 있습니다.
-    // 이 부분은 실제 예약 로직을 구현해야 합니다.
-    alert(`좌석 예약을 진행합니다.`);
-    handleReservation();
-    closeModal();
+    if (!studentId) {
+      alert("학번을 입력하세요.");
+    } else {
+      alert(`학번 ${studentId}으로 ${content} 좌석 예약을 진행합니다.`);
+      closeModal();
+      onReserve(); // 추가: 예약 버튼 클릭 시 호출하여 의자 예약 상태 업데이트
+    }
   };
 
   return (
-    <Modal isOpen={isOpen} onRequestClose={closeModal}>
-      <h2>좌석을 예약하시겠습니까?</h2>
-      <button onClick={handleReserveClick} onChange={handleStudentIdChange}>예약</button>
-      <button onClick={closeModal}>닫기</button>
+    <Modal isOpen={isOpen} onRequestClose={closeModal} className="ResModal">
+      <div className={`ModalContent ${isChairReserved ? 'reserved' : ''}`}> {/* 수정: 예약 상태에 따라 클래스 추가 */}
+        <h1>{content}</h1>
+        <h2>좌석을 예약하시겠습니까?</h2>
+        <p>학번을 입력하세요</p>
+        <input className="stdnum" type="text" value={studentId} onChange={handleStudentIdChange} />
+        <button className="res" onClick={handleReserveClick}>예약</button>
+        <button className="notres" onClick={closeModal}>닫기</button>
+      </div>
     </Modal>
   );
 }
 
-export default OneSeatModal;
+export default ResModal;
